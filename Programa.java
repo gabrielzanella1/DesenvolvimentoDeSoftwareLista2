2a7bd536da6bf947fead5148dcefdccadee9459e
Gabriel <gzanella202@gmail.com>
2024-03-18 16:04:09 -0300

    Exercicio14

--- Programa.java
+++ Programa.java
@@ -52,13 +52,13 @@ public class Programa {
         Exercicio12.executar();
         */
 
-        
-        Exercicio13.executar();
-        
-
         /* 
-        Exercicio14.executar();
+        Exercicio13.executar();
         */
+
+        
+        Exercicio14.executar();
+    
 
         /* 
         Exercicio15.executar();
